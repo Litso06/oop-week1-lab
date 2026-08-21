bb85f93686501e0b3ed63f40e5895e2f68108253
Madalitso Lungu <madalitsolungu06@gmail.com>
2026-08-21 10:50:32 +0200

    Add Week 1 lab files

---
+++ Person.java
@@ -0,0 +1,25 @@
+class Person {
+    String name;
+    int age;
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    void printDetails() {
+        System.out.println(name + " is " + age + " years old");
+    }
+
+    public static void main(String[] args){
+        Person[] people = {
+            new Person("Ben", 10),
+            new Person("Mary", 6),
+            new Person("Leo", 97),
+            new Person("Mambwe", 41)
+        };
+
+        for (Person p : people) {
+            p.printDetails();
+        }
+    }
+}
+
---
+++ procedural.java
@@ -0,0 +1,13 @@
+public class procedural {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    
+
+        String[] names = {"Ben", "Mary", "Leo"};
+        int[] ages = {10, 6}; 
+
+        for(int i = 0; i < names.length; i++) {    
+            System.out.println(names[i] + " is " + ages[i] + " years old"); 
+        }
+    }
+}
